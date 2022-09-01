71986e6b0e59a7a7ee817ace07fc9f4184a86a5e
havoriskova <ha.voriskova@gmail.com>
2022-09-01 23:03:56 +0200

    update
    - changed padding on header (I know, it is bit of overkill, but this approach would be handy if the background would be more complicated AND you want to have invisible background)
    - fix active-nav with width for phones

--- src/Header.js
+++ src/Header.js
@@ -15,10 +15,13 @@ function respNav () {
 
 }
 
+
 function scrollFun (e) {
 let idName = e.target.attributes[0].value;
 let element = document.getElementById(idName);
 element.scrollIntoView({behavior: "smooth"});
+setActiveNav(null);
+setActiveIcon(null);
 }
 
 
